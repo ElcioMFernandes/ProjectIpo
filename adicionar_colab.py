32baeecb0897176bdc137f08aa8e653272f915af
Élcio Mateus Fernandes <126964747+ElcioMFernandes@users.noreply.github.com>
2023-10-31 20:32:03 -0300

    Add files via upload

---
+++ adicionar_colab.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+def conn_db():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='1234',
+        database='db_ipo'
+        )
+
+    try:
+        cursor = conn.cursor()
+        if conn.is_connected():
+            print('Conectado ao banco de dados')
+            return cursor, conn
+        else:
+            print('Não foi possível conectar ao banco de dados')
+            return None
+            
+    except Exception as e:
+        print('Erro ao conectar com o banco de dados: ', e)
+        return None
+
+conn_db()
